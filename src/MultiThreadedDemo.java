4ffad9fe0aa82b225c9ca7656e2859a0f90e8d4e
黃鼎文 <aa6413779@hotmail.com.tw>
2023-06-13 16:10:53 +0800

    Java Multi-threaded Programming Demo

---
+++ src/MultiThreadedDemo.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//launch threads (Class implements Runnable interface)
+class PrintNumber implements Runnable {
+    private int max;
+    public PrintNumber(int max) {
+        this.max = max;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i <max; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
+class PrintChar implements Runnable {
+    private int times;
+    private char aChar;
+    public PrintChar(char aChar,int times) {
+        this.aChar = aChar;
+        this.times = times;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i <times; i++) {
+            System.out.print(aChar);
+        }
+    }
+}
+public class MultiThreadedDemo {
+
+    public static void main(String[] args) {
+//        Thread thread1 = new Thread(new PrintNumber(50));
+//        Thread thread2 = new Thread(new PrintNumber(50));
+//        Thread thread3 = new Thread(new PrintChar('a', 50));
+//
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.execute(new PrintChar('a', 20));
+        executorService.execute(new PrintChar('b', 20));
+        executorService.execute(new PrintChar('c', 20));
+        executorService.execute(new PrintNumber(20));
+
+        executorService.shutdown();
+    }
+}
