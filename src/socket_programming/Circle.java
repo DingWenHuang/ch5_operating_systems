755450cb4cdc485af6c0364fdb73b2d9d39438b7
黃鼎文 <aa6413779@hotmail.com.tw>
2023-06-13 16:43:12 +0800

    Socket Programming - Sending Objects

---
+++ src/socket_programming/Circle.java
@@ -0,0 +1,15 @@
+package socket_programming;
+
+import java.io.Serializable;
+
+public class Circle implements Serializable {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+}
